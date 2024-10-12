616e893e78b8d249d322814af70ecc79c1b09881
mizan <mizanurrahmanmizan934@gmail.com>
2024-10-12 23:45:56 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+/* 
+
+console.log(2+2);
+console.log('hgbgxmjnkjhb');   
+console.log('mizanur rahman')
+
+
+document.write("Hello");
+document.write(58+51);
+*/
+
+/*
+alert('Hello! Mizanur');
+alert(85+84);
+
+
+---run code with node----
+node script.js
+
+*/
+
