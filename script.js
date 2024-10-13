6fb340d9bfe0f84016c0da5995456468f3f3bc35
mizan <mizanurrahmanmizan934@gmail.com>
2024-10-13 12:38:52 +0600

    variable etc

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-/* 
+
 
 console.log(2+2);
 console.log('hgbgxmjnkjhb');   
@@ -8,13 +8,15 @@ console.log('mizanur rahman')
 
 document.write("Hello");
 document.write(58+51);
-*/
 
-/*
+
+
 alert('Hello! Mizanur');
 alert(85+84);
 
 
+
+/*
 ---run code with node----
 node script.js
 
---
+++ variable.js
@@ -0,0 +1,32 @@
+
+
+var mynum=122;
+console.log(typeof mynum);
+
+var myName = 'akash';
+console.log(typeof myName);
+
+var mybol = true;
+console.log(typeof mybol);
+
+var myStr = [1,2,'miss'];
+console.log(typeof myStr);
+
+
+var nameStr = ['ruby','sumi','jami'];
+console.log(nameStr.length);
+
+console.log(nameStr.indexOf('sumi'));
+
+console.log(nameStr[2]);
+
+//console.log(mynum,myName,mybol,myStr);
+
+
+//push And pop
+
+var friendName = ['karim','rahim','sakib','rakib'];
+
+//friendName.push('bilkis')
+friendName.pop()
+ console.log(friendName);
