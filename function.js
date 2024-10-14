249de37faa526b04231f5e8553ec3ac16640f639
mizan <mizanurrahmanmizan934@gmail.com>
2024-10-14 12:33:36 +0600

    funtion declar

---
+++ function.js
@@ -0,0 +1,23 @@
+
+//funcion declartion
+function myFun(){
+    console.log("ammr nam Mizanur and I'm form chattogram ");
+}
+myFun()
+
+
+
+function myFun2(name, location){
+    console.log("ammr nam " +name+ " and I'm form "+location);
+}
+myFun2("Rahim","comilla")
+
+//add two number with function
+
+function myFun3(num1,num2){
+    let total = num1+num2;
+    return total;
+}
+let result=myFun3(3,6);
+console.log(result);
+
