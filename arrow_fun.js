02218a4202e5f283c4166c2d0dcfd5a9068d768b
mizan <mizanurrahmanmizan934@gmail.com>
2024-10-14 23:35:56 +0600

    arrow fun

---
+++ arrow_fun.js
@@ -0,0 +1,12 @@
+//  function simpleFun(num1,num2){
+//     return num1+num2;
+//  }
+// console.log(simpleFun(20,30));
+
+//Arrow function
+let arrowFun=(num1,num2)=>{
+    return num1+num2;
+}
+console.log(arrowFun(20,30));
+
+let arrowFun1=(num1,num2)=> num1+num2;
