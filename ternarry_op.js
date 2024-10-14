dba0213ca88138d2205bb022754d1f8dfabfb38c
mizan <mizanurrahmanmizan934@gmail.com>
2024-10-14 11:09:55 +0600

    ternarry op

---
+++ ternarry_op.js
@@ -0,0 +1,17 @@
+
+let x=10;
+ 
+if(x==6){
+    console.log("school vorte hote parbe");
+}
+else{
+    console.log("school vorte hote parbe na");
+}
+
+ y=(x==6)?"school vorte hote parbe" :"school vorte hote parbe na";
+ console.log(y);
+
+ let c;
+ (x==6)?c="school vorte hote parbe" :c="school vorte hote parbe na";
+ console.log(c);
+ 
