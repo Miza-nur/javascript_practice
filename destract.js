405b2460168f9ddd91552135500b5c1ce647951e
mizan <mizanurrahmanmizan934@gmail.com>
2024-10-15 17:24:20 +0600

    destracturing arr

--- destract.js
+++ destract.js
@@ -1,10 +1,13 @@
+
 const addCard={
     id: 1,
     name:"ksgvcf",
     price:123
 
 }
-
 const {id,name,price}= addCard;
+console.log(name);
 
-console.log(id);
+const names =["younus","mizan","hero","arif"];
+const [nam1,nam2,nam3,nam4]= names;
+console.log(nam4);
