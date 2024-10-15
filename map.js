d16f760a689d5fd28fa501c953ba9eff66910e65
mizan <mizanurrahmanmizan934@gmail.com>
2024-10-15 12:12:36 +0600

    map fun

---
+++ map.js
@@ -0,0 +1,14 @@
+const numbers =[1,5,8,9,7,5,3,6];
+
+
+numbers.map((number)=>{
+    console.log(number*2);
+
+});
+
+// const result=numbers.map((number)=>{
+//     return number*2;
+
+// });
+
+// console.log(result);
