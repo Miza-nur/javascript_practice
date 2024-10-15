8c875616e92086e842e2a9ac0042dd5d51889ac1
mizan <mizanurrahmanmizan934@gmail.com>
2024-10-15 10:03:02 +0600

    forEach

---
+++ forEach.js
@@ -0,0 +1,17 @@
+
+const names =["younus","mizan","hero","arif"];
+
+names.forEach((name)=>{
+    console.log(name);
+
+});
+
+
+
+
+//regular function
+
+// names.forEach(nam);
+// function nam(name){
+//     console.log(name);
+// }
