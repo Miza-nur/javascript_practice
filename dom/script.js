cbbf61f281836ac2891b6adac7377bc2e44d1a06
mizan <mizanurrahmanmizan934@gmail.com>
2024-10-16 23:12:40 +0600

    querySelector

--- dom/script.js
+++ dom/script.js
@@ -2,3 +2,5 @@ function printvalue(){
     var name=document.form1.name.value;  
     alert("Welcome: "+name);  
     }  
+
+const result=document.querySelector("#hello").innerText="Welcome Programmer"
