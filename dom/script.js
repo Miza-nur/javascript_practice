2e6284d51c7a05af51e81ede67fc44919b88fc1d
mizan <mizanurrahmanmizan934@gmail.com>
2024-10-17 09:44:03 +0600

    append element

--- dom/script.js
+++ dom/script.js
@@ -3,8 +3,28 @@ function printvalue(){
     alert("Welcome: "+name);  
     }  
 
-    //querySelector
+//querySelector
 const result=document.querySelector("#hello").innerText="Welcome Programmer"
 
 // css with seclector
 document.querySelector("#hello").style.color="red";
+
+//append element.....
+
+let appendEle =document.getElementById("bodyId");
+
+let div =document.createElement("div");
+div.innerHTML=`
+<h1>This is from javascript</h1>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit assumenda nesciunt praesentium dolor temporibus corrupti, aut in quaerat neque,</p>
+
+<lu>
+<li>List-1</li>
+<li>List-2</li>
+<li>List-3</li>
+<li>List-4</li>
+
+</lu>
+`,
+
+bodyId.appendChild(div);
