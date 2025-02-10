b59906f0016f1c946e819f1d90a871331a473285
mizan <mizanurrahmanmizan934@gmail.com>
2025-02-10 22:35:56 +0600

    :star:

--- Dom_selector/script.js
+++ Dom_selector/script.js
@@ -1,6 +1,17 @@
+// getElementsByTagName
+
 const findTag= document.getElementsByTagName('li');
 console.log(findTag);
 
 for(let i=0;i<findTag.length;i++){
     console.log(findTag[i]);
 }
+
+// getElementsById
+const findId=document.getElementById('hello');
+console.log(findId);
+const findId1=document.getElementById('helloo');
+console.log(findId1);
+
+// getElementsByClassName
+const findclass =document.getElementsByClassName('')
