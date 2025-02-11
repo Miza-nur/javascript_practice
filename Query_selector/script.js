b36d58e7590085ce4254a8b378faca1e05dc6294
mizan <mizanurrahmanmizan934@gmail.com>
2025-02-11 21:53:38 +0600

    :star::star:

--- Query_selector/script.js
+++ Query_selector/script.js
@@ -2,4 +2,14 @@
 // querySelector
 
 const selectQuery=document.querySelector('#spantag').innerText='Hello programmer';
-console.log(selectQuery)
+console.log(selectQuery);
+const selectQuery2=document.querySelector('#ptag');
+console.log(selectQuery2.innerHTML='<h>This is Selector</h>');
+
+
+// querySelectorAll
+const selectQuery3=document.querySelectorAll('.List-1');
+
+for(let i=0;i<selectQuery3.length;i++){
+    console.log(selectQuery3[i].innerText='Hello');
+}
