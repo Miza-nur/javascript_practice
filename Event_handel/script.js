ffb6a0bab02d02bd645bdb319051f7b57f3d4d83
mizan <mizanurrahmanmizan934@gmail.com>
2025-02-15 20:04:17 +0600

    :star::start:

--- Event_handel/script.js
+++ Event_handel/script.js
@@ -2,3 +2,7 @@ const heading=document.getElementById('heading');
 function headfunc(){
     heading.innerText='This is my Heading'
 }
+
+function forDate(){
+    document.getElementById('date').innerText = Date();
+}
