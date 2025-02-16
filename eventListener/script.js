c910f300ce8b8558757b46629b57383aa7016c96
mizan <mizanurrahmanmizan934@gmail.com>
2025-02-16 22:43:37 +0600

    :star:

--- eventListener/script.js
+++ eventListener/script.js
@@ -6,4 +6,13 @@ document.getElementById('update-btn').addEventListener('click',function(){
     defaultText.innerText=inputfield.value;
 
     inputfield.value='';
+
+})
+
+
+
+const loginbtn =document.getElementById('Login');
+loginbtn.addEventListener('click',function(){
+    loginbtn.innerText='Logout';
+    loginbtn.style.backgroundColor='green';
 })
