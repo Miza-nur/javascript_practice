02b1ba3bbb6126833c9ff567058db8a4ebe67f53
mizan <mizanurrahmanmizan934@gmail.com>
2025-02-22 12:53:22 +0600

    :star::star:

--- api_json/script.js
+++ api_json/script.js
@@ -1,14 +1,19 @@
 function dataLoad(){
-    fetch('https://jsonplaceholder.typicode.com/comments?postId=1')
+    fetch('https://jsonplaceholder.typicode.com/posts')
     .then(Response => Response.json())
     .then(data => dataDisplay(data))
 }
-
+dataLoad();
 function dataDisplay(data){
     const div=document.getElementById('divTag')
+
     data.map(user=>{
-        const p=document.createElement('p')
-        p.innerText=user.name;
+        const p=document.createElement('div')
+        p.innerHTML=`
+        <h4>ID: ${user.id}</h4>
+        <h2>Title: ${user.title}</h2>
+        <p>Body: ${user.body}</p>
+        `;
         div.appendChild(p);
     });
 }
